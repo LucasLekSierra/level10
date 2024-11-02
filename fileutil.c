d6a1bf01fdfb1c61d2d683e06ad940aff346f33d
Lucas Lek <llek@student.sierracollege.edu>
2024-11-02 13:09:51 -0700

    Implement util functions (array of arrays).

--- fileutil.c
+++ fileutil.c
@@ -25,6 +25,13 @@ char ** loadFileAA(char *filename, int *size)
 	    perror("Can't open file");
 	    exit(1);
 	}
+
+	int capacity = 50;
+	char ** arr = malloc(capacity * sizeof(char *));
+	char string[1000];
+	
+	// The size should be the number of entries in the array.
+	*size = 0;
 	
 	// TODO
 	// Allocate memory for an array of strings (arr).
@@ -35,12 +42,34 @@ char ** loadFileAA(char *filename, int *size)
 	//   Copy each line into the string (use strcpy).
 	//   Attach the string to the large array (assignment =).
     // Close the file.
-	
-	// The size should be the number of entries in the array.
-	*size = 0;
+
+	while (fgets(string, 1000, in) != NULL)
+	{
+		// if we are above capacity, expand the array
+		if (*size >= capacity)
+		{
+			capacity *= 2;
+			arr = realloc(arr, capacity * sizeof(char *));
+		}
+
+		// trim the newline
+		for (int i = 0; i < strlen(string); i++)
+			if (string[i] == '\n')
+				string[i] = '\0';
+
+		// allocate memory for the string
+		arr[*size] = malloc(strlen(string) + 1);
+
+		// copy the string into the array
+		strcpy(arr[*size], string);
+		// add it to the count
+		*size = *size + 1;
+	}
+
+	fclose(in);
 	
 	// Return pointer to the array of strings.
-	return NULL;
+	return arr;
 }
 
 char (*loadFile2D(char *filename, int *size))[COLS]
@@ -71,6 +100,14 @@ char (*loadFile2D(char *filename, int *size))[COLS]
 // Return the found string or NULL if not found.
 char * substringSearchAA(char *target, char **lines, int size)
 {
+	char * foundString;
+	int pos = 0;
+	for (int i = 0; i < size; i++)
+	{
+		foundString = strstr(lines[i], target);
+		if (foundString != NULL)
+			return lines[i];
+	}
 
 	return NULL;
 }
@@ -84,7 +121,10 @@ char * substringSearch2D(char *target, char (*lines)[COLS], int size)
 // Free the memory used by the array
 void freeAA(char ** arr, int size)
 {
+	for (int i = 0; i < size; i++)
+		free(arr[size]);
 
+	free(arr);
 }
 
 void free2D(char (*arr)[COLS])
